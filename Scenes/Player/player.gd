d9caa6423a46095c190e9b6c25f073e3d93c735c
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-02-23 19:53:12 +0100

    Player animations

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -5,14 +5,17 @@ extends CharacterBody2D
 @export var acceleration := 56.0
 
 @onready var sprite := $Sprite
+@onready var animation_player := $AnimationPlayer
+
+var direction := Vector2.ZERO
 
 
 func _physics_process(_delta: float) -> void:
 	handle_movement()
+	handle_animation()
 
 
 func handle_movement() -> void:
-	var direction := Vector2.ZERO
 	direction.x = Input.get_axis("left", "right")
 	direction.y = Input.get_axis("up", "down")
 	direction = direction.normalized()
@@ -22,3 +25,15 @@ func handle_movement() -> void:
 		velocity = Vector2.ZERO
 	
 	move_and_slide()
+
+
+func handle_animation() -> void:
+	if direction.x < 0:
+		sprite.flip_h = true
+	elif direction.x > 0:
+		sprite.flip_h = false
+	
+	if direction:
+		animation_player.play("run")
+	else:
+		animation_player.play("idle")
