c772190859043886b29f6b44ead2686e27ef5d5e
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-02-24 22:03:37 +0100

    Working on enemies

--- Scenes/Enemies/ujist.gd
+++ Scenes/Enemies/ujist.gd
@@ -5,33 +5,35 @@ extends CharacterBody2D
 @export var glory_min: int
 @export var glory_max: int
 
-var malfunction := false
+var direction: Vector2
 
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var effects := $Effects
+@onready var sprite := $Sprite
 
 
 func _ready() -> void:
 	health_component.health_depleted.connect(_on_health_depleted)
 	health_component.health_changed.connect(_on_health_changed)
 	
-	# Possibility of malfunction ability
-	if GameController.abilities["BAThesis"]["acquired"]:
-		if randi_range(1, 100) <= GameController.abilities["BAThesis"]["malfunction"]:
-			malfunction = true
+	direction = Vector2.from_angle(randf_range(0.0, TAU))
 
 
-func _physics_process(_delta: float) -> void:
-	move_towards_player()
-
-
-func move_towards_player() -> void:
-	var direction := global_position.direction_to(GameController.player.global_position)
-	look_at(GameController.player.global_position)
-	velocity = direction * speed
-	move_and_slide()
+func _physics_process(delta: float) -> void:
+	move(delta)
 	
-	# TODO: Add explosion if malfunction=true
+	if velocity.x < 0:
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
+
+
+func move(delta: float) -> void:
+	velocity = direction * speed
+	var collision := move_and_collide(velocity * delta)
+	if collision:
+		var bounce_velocity := velocity.bounce(collision.get_normal())
+		velocity = bounce_velocity
 
 
 func _on_health_changed() -> void:
